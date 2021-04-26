2d61995cf33af97b7d6aace41c754fdf1db7fabb
Xss721 <844841121@qq.com>
2021-04-26 14:30:59 +0800

    Initial commit

---
+++ src/main/java/com/xss/MainApplication.java
@@ -0,0 +1,11 @@
+package com.xss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
+    }
+}
